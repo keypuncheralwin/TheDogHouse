b35cd82e1f1aa044cbf96e061335b0715cce16e4
Alwin George <alwingeorge11@gmail.com>
2021-10-24 17:13:53 +1030

    started on the navbar

---
+++ client/js/navbar.js
@@ -0,0 +1,21 @@
+
+function createNav(){
+    const navBar = document.getElementById('navBar')
+    
+    
+    const signUp = document.createElement('div')
+    signUp.textContent = 'Sign Up'
+    signUp.classList.add('navLink')
+    navBar.append(signUp)
+    
+    const login = document.createElement('div')
+    login.textContent = 'Login'
+    login.classList.add('navLink')
+    navBar.append(login)
+    
+    const addPet = document.createElement('div')
+    addPet.textContent = 'Add a Pet'
+    addPet.classList.add('navLink')
+    navBar.append(addPet)
+}
+
