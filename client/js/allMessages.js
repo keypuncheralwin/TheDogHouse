70ad4b914c660c5b09602ee378f251db9f796b94
laraluitingh <61213237+laraluitingh@users.noreply.github.com>
2021-11-03 15:58:16 +1100

    changes

--- client/js/allMessages.js
+++ client/js/allMessages.js
@@ -1,35 +1,49 @@
+function getAllMessages() {
+  axios.get(`api/messages/getAllMessages`).then((allData) => {
+    peopleUserIsChattingTo = allData.data.array;
+    console.log(peopleUserIsChattingTo);
+    const container = document.getElementById("container");
+    console.log(container);
+    container.innerHTML = "";
 
-function getAllMessages(){
-    axios.get(`api/messages/getAllMessages`).then((allData)=>{
-        peopleUserIsChattingTo=allData.data.array
-        console.log(peopleUserIsChattingTo)
-        const container = document.getElementById("container");
-        console.log(container);
-        container.innerHTML = "";
+    // getuserDetails= `api/messages/user/${i}`
+    // getMessageDetails=`api/messages/getMessages/${i}`
 
-        for(i of peopleUserIsChattingTo){
-            console.log(i)
-            axios.get(`api/messages/user/${i}`).then((user)=>{
-                console.log(user.data)
-                div=document.createElement('div')
-                user_name=document.createElement('p')
-                user_name.textContent=user.data[0].name
-                div.append(user_name)
+    for (i of peopleUserIsChattingTo) {
+      console.log(i);
+      div = document.createElement("div");
+      getuserDetails = `api/messages/user/${i}`;
+      getMessageDetails = `api/messages/getMessages/${i}`;
+      requestOne = axios.get(getuserDetails);
+      requestTwo = axios.get(getMessageDetails);
+      axios.all([requestOne, requestTwo]).then(
+        axios.spread((...responses) => {
+          const usersInfo = responses[0].data;
+          console.log(usersInfo)
+          const arrMessages = responses[1].data;
+          console.log(arrMessages)
 
-                axios.get(`api/messages/getMessages/${i}`).then((arrMessages)=>{
-                    lastItem=arrMessages.data.slice(-1)
-                    console.log(lastItem[0])
-                    lastText=document.createElement('p')
-                    lastText.textContent=lastItem[0].body
-                    div.append(lastText)
+          user_name = document.createElement("p");
+          user_name.textContent = usersInfo[0].name;
+          div.append(user_name);
 
-                    container.append(div)
 
-                })
+          lastItem = arrMessages.slice(-1);
+          console.log(lastItem);
+          console.log(lastItem[0]);
+          lastText = document.createElement("p");
+          lastText.textContent = lastItem[0].body;
+          div.appendChild(lastText);
 
-            })
-              
-        }
+          div.addEventListener(("click"), (e)=>{
+              getAllMessagesBetweenUsers(usersInfo[0])
+          })
+          container.append(div)
+        })
+      );
 
-    })
+      // console.log(val)
+    }
+  })
 }
+
